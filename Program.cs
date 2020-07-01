2255de80cc077c7716496d2528a77ae17064089a
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-01 13:46:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,10 +9,10 @@ namespace dz1
 
             int pictures = 52;
             int picturesInRow = 3;
-            int allPicturesInRow = pictures / picturesInRow;
+            int allPicturesInRows = pictures / picturesInRow;
             int picturesOutRow = pictures % picturesInRow;
 
-            Console.WriteLine($"Всего картин - {pictures}. \nПолностью заполненных рядов картин получилось - {allPicturesInRow}. " +
+            Console.WriteLine($"Всего картин - {pictures}. \nПолностью заполненных рядов картин получилось - {allPicturesInRows}. " +
                 $"\nОстаток картин - {picturesOutRow}.");
             Console.ReadKey();
         }
