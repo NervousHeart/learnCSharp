b499e09ff952075789b6c532cebe9400f4a19563
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-01 14:09:34 +0300

    Add files via upload

---
+++ dz7.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение, которые хотите получить:");
+            string messege = Console.ReadLine();
+            Console.WriteLine("Введите сколько раз нужно повторить ваше сообщение");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(messege);
+            }
+        }
+    }
+}
