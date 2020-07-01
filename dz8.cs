12e67e8a3e790e1c28bc51d96fe3cfcfd9a3f4c7
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-01 15:13:08 +0300

    Add files via upload

---
+++ dz8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool workProg = true;
+                 while (workProg)
+             {
+                Console.WriteLine("Программа работает, если вы введете любое слово - программа все так же будет работать");
+                Console.ReadLine();
+                Console.WriteLine("Чтобы выйти из программы введите - \"exit\"");
+                string exit = Console.ReadLine();
+                if (exit=="exit")
+                {
+                    workProg = false;
+                }
+             }
+        }
+    }
+}
