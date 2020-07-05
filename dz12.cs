99a83370c519b0b91e2c08605aaaa3764319d92c
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-05 14:12:59 +0300

    Add files via upload

---
+++ dz12.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            char symbol;
+            string row;
+
+            Console.WriteLine("Введите имя:");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите символ");
+            symbol = Convert.ToChar(Console.ReadLine());
+            row = Convert.ToString(symbol);
+            for (int i = 0; i <= name.Length; i++)
+            {
+                row += symbol;
+            }
+            Console.WriteLine(row + 
+                $"\n{symbol}{name}{symbol}" +
+                $"\n{row}");
+
+        }
+    }
+}
