72de302b90c2953ee1d0db69e53f4a6995e91b4d
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-05 13:39:37 +0300

    Add files via upload

---
+++ dz13.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "perv1";
+            string enterPassword;
+
+
+                for (int i = 0; i < 4;)
+                {
+                    Console.WriteLine("\nВведите пароль:");
+                    enterPassword = Console.ReadLine();
+                    switch (enterPassword)
+                    {
+                        case "perv1":
+                            Console.WriteLine("Очень тайное сообщение");
+                        i = 0;
+                            break;
+                        default:
+                            Console.WriteLine($"Неверный пароль, у вас осталось {3 - i} попытки");
+                        i++;
+                            break;
+                    }
+                }
+
+        }
+    }
+}
