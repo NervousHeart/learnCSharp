25287bd293b58641f990088ed2f98bbc48c4f2f8
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-05 13:20:17 +0300

    Add files via upload

---
+++ dz11.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 7;
+            while (number <= 98) // Здесь был выбран цикл while потому что известно конечное число последовательности и ее шаг. 
+            {
+                Console.Write(number + " ");
+                number += 7;
+            }
+        }
+    }
+}
