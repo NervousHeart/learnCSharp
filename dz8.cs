b24f969e16b0ff79c0255f562895fd511def6acc
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-06 09:34:08 +0300

    Update dz8.cs

--- dz8.cs
+++ dz8.cs
@@ -11,10 +11,10 @@ namespace dz1
              {
                 Console.WriteLine("Здравствуйте! Программа складывает любые целые числа!" +
                     "\nВведите первое целое число:");
-                int a = Convert.ToInt32(Console.ReadLine());
+                int firstNumber = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Введите второе число:");
-                int b = Convert.ToInt32(Console.ReadLine());
-                int sum = a + b;
+                int secondNumber = Convert.ToInt32(Console.ReadLine());
+                int sum = firstNumber + secondNumber;
                 Console.WriteLine("Сумма двух чисел равна: " + sum);
                 Console.WriteLine("Чтобы выйти из программы введите - \"exit\"");
                 string exit = Console.ReadLine();
