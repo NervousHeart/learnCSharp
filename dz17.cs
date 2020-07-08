cd245b7e4a8b0661071ace8fd2a77a477b731484
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-08 14:37:43 +0300

    Add files via upload

---
+++ dz17.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[userValue];
+            int sum = 0;
+            Console.WriteLine();
+
+            Console.WriteLine($"Введите {userValue} чисел ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+                sum += array[i];
+            }
+            Console.WriteLine($"\nСумма чисел в массиве равна {sum}");
+        }
+    }
+}
