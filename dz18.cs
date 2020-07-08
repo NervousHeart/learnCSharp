f35d0a76a95275d5a2f479d53e3b2782a73c04f3
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-08 14:50:43 +0300

    Update dz18.cs

--- dz18.cs
+++ dz18.cs
@@ -7,10 +7,13 @@ namespace ConsoleApp9
     {
         static void Main(string[] args)
         {
+           bool workProg = true;
+
             Console.WriteLine("Введите количество чисел");
             int userValue = Convert.ToInt32(Console.ReadLine());
             int[] array = new int[userValue];
             int sum = 0;
+            string userInPut;
             Console.WriteLine();
 
             Console.WriteLine($"Введите {userValue} чисел ");
@@ -18,12 +21,33 @@ namespace ConsoleApp9
             {
                 array[i] = Convert.ToInt32(Console.ReadLine());
             }
-            for (int i = 0; i < array.Length; i++)
+            while (workProg)
             {
-                Console.Write(array[i] + " ");
-                sum += array[i];
+
+                Console.WriteLine("1. Чтобы сложить все числа массива введите команду \"sum\"" +
+                    "\n2. Для того чтобы завершить программу введите команду \"exit\"");
+
+                userInPut = Console.ReadLine();
+
+                if (userInPut == "sum")
+                {
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        Console.Write(array[i] + " ");
+                        sum += array[i];
+                    }
+                    Console.WriteLine($"\nСумма чисел в массиве равна {sum}");
+                }
+                else if (userInPut == "exit")
+                {
+                    workProg = false;
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестная команда. Проверьте правильность ввода команды.");
+                }
+
             }
-            Console.WriteLine($"\nСумма чисел в массиве равна {sum}");
         }
     }
 }
