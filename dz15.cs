e717a571ab27a85c685e0689f5995f04fc41cf07
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-09 09:55:09 +0300

    Update dz15.cs

--- dz15.cs
+++ dz15.cs
@@ -6,29 +6,42 @@ namespace ConsoleApp6111
     {
         static void Main(string[] args)
         {
+      
             int[,] array = new int[5, 5] ;
             Random rand = new Random();
             int sumSecondRow = 0; int multiplicationOneColumn = 1;
+            
+            //заполнение массива
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
                     array[i, j] = rand.Next(0, 10);
                 }
-                    multiplicationOneColumn *= array[i, 0];
             }
             
+            //операции с ячейками массива
+                for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                }
+                    multiplicationOneColumn *= array[i, 0];
+                    sumSecondRow += array[1, i];
+            }
+            
+            //вывод массива
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
                 Console.Write(array[i,j] + " ");
                 }
-                sumSecondRow += array[1, i];
                 Console.WriteLine();
             }
                 Console.WriteLine("произведение первого столбца: " + multiplicationOneColumn);
                 Console.WriteLine("сумма второй строки: " + sumSecondRow);
+
         }
     }
 }
