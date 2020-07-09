b7dbdaee62cff13ff305a7fb2397cf5a3e7d8594
NervousHeart <67158400+NervousHeart@users.noreply.github.com>
2020-07-09 10:35:10 +0300

    Update dz15.cs

--- dz15.cs
+++ dz15.cs
@@ -11,34 +11,29 @@ namespace ConsoleApp6111
             Random rand = new Random();
             int sumSecondRow = 0; int multiplicationOneColumn = 1;
             
-            //заполнение массива
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
                     array[i, j] = rand.Next(0, 10);
-                }
-            }
-            
-            //операции с ячейками массива
-                for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                }
-                    multiplicationOneColumn *= array[i, 0];
-                    sumSecondRow += array[1, i];
-            }
-            
-            //вывод массива
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                Console.Write(array[i,j] + " ");
+                    Console.Write(array[i,j] + " ");
                 }
                 Console.WriteLine();
             }
+            
+                for (int i = 0; i < array.GetLength(0);i++)
+            {
+                    multiplicationOneColumn *= array[i, 0];
+            }
+            
+                for (int i = 1; i < 2; i++)
+                {
+                    for (int j = 0; j< array.GetLength(1);j++)
+                    {
+                        sumSecondRow+= array[i,j];
+                    }
+                }
+            
                 Console.WriteLine("произведение первого столбца: " + multiplicationOneColumn);
                 Console.WriteLine("сумма второй строки: " + sumSecondRow);
 
